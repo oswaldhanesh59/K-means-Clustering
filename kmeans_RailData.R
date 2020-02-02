6eadedff47ab12146af8c95aef44e115dc4ad39f
oswaldhanesh59 <60353733+oswaldhanesh59@users.noreply.github.com>
2020-02-02 18:21:54 +0530

    Add files via upload

---
+++ kmeans_RailData.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+rd <- read.csv(file.choose(),header = T)
+str(rd)
+
+scaled.dat <- scale(rd[,-1])
+View(scaled.dat)
+
+scaled.dat<- as.data.frame(cbind(rd[,1],scaled.dat))
+names(scaled.dat)
+
+set.seed(20)
+cluster_output <- kmeans(scaled.dat[,-1], 4)
+
+wss <- (nrow(scaled.dat[,-1])-1)*sum(apply(scaled.dat[,-1],2,var))
+
+for (i in 2:15) wss[i] <- sum(kmeans(scaled.dat[,-1],centers=i)$withinss)
+plot(1:15, wss, type='b', xlab='Number of Clusters', ylab='Within groups sum of squares',col='mediumseagreen',pch=12)
+
+str(cluster_output)
+cluster_output$centers
+
+cluster_output
+
+names(cluster_output)
+
+library(ggplot2)
+ggplot(cluster_output,aes(Average.Kms.Per.Day,Passenger.Kilometers,color=as.factor(clusters)))+geom_point()
+
